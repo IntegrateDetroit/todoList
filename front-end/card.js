66a45a624ea26568b393a4e4fc7fa473ad315f09
Stephanie Vaughn <stephanie.vaughn@gmail.com>
2018-11-09 18:59:52 -0500

    new list title is centered in list card; added pencil icon for editing title.

--- front-end/card.js
+++ front-end/card.js
@@ -7,7 +7,16 @@ var card = {
       <div class="large-6 large-pull-6 cell">
        <div class="card card-tabs large-6">
         <div class="card-divider">
-          <h6 class="listCardTitle">${newTitle} </h6>
+
+        <div class="listCardTitle">
+          <h6 style="display: inline;">${newTitle}</h6>
+
+          <button style="opacity: 0.5; padding-left: 10px; padding-bottom: 15px;">
+            <img src="./resources/pen-solid.svg" width="15px">
+          </button>
+          </div>
+
+          
           <button href="#" data-close data-reveal-id="archiveModal" class="secondary button" id="archiveModal" type="submit">Archive List
             <img src="./resources/iconmonstr-gear-1.svg">
           </button>
@@ -19,9 +28,9 @@ var card = {
    
     $("body").append(todoCard);
     const card = document.querySelector("#"+cardId);
-    card.addEventListener('click', function (e) {
-      card.remove()
-      })
+    // card.addEventListener('click', function (e) {
+    //   card.remove()
+    //   })
     currId++;
   }
 
