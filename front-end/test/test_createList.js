d4b720bfb628cb9855cf857a54e44dc3c6291f55
Samah Majadla <samah@Samahs-MBP.hsd1.mi.comcast.net>
2018-11-12 16:57:10 -0500

    cole helped fix front end tests so that the actions happened prior to the assertions on things that the actions created

--- front-end/test/test_createList.js
+++ front-end/test/test_createList.js
@@ -1,24 +1,24 @@
 
 
-// QUnit.test("list title field initially 'Enter title here'", function() {
-//   const input = document.querySelector('#titleInput');
+QUnit.test("list title field initially 'Enter title here'", function() {
+  const input = document.querySelector('#titleInput');
 
-//   equal(input.placeholder, "Enter title here", "Initial list title placeholder is presents");
-// });
+  equal(input.placeholder, "Enter title here", "Initial list title placeholder is presents");
+});
 
-// QUnit.test("button click sends information to DB", function() {
-//   var spy = sinon.spy(request, "post");
-//   const input = document.querySelector('#titleInput');
+QUnit.test("button click sends information to DB", function() {
+  var spy = sinon.spy(request, "post");
+  const input = document.querySelector('#titleInput');
 
-//   var expectedTitle = "this is new title";
-//   input.value = expectedTitle;
-//   var button = document.querySelector('#createListButton').click();
-//   equal(spy.args[0][0].title, expectedTitle, "title was as expected");
-//   equal(spy.args[0][1], "http://localhost:8080/list");
-//   spy.restore();
-// });
+  var expectedTitle = "this is new title";
+  input.value = expectedTitle;
+  var button = document.querySelector('#createListButton').click();
+  equal(spy.args[0][0].title, expectedTitle, "title was as expected");
+  equal(spy.args[0][1], "http://localhost:8080/list");
+  spy.restore();
+});
 
-QUnit.test("button click sends updated title to DB", function() {
+QUnit.test("button click sends updated title to DB", async function() {
   var spy = sinon.spy(request, "post");
   //create a list
   var idForList = 3
@@ -27,7 +27,6 @@ QUnit.test("button click sends updated title to DB", function() {
   var listTitleEntered = "new list"
   titleForNewCard.value = listTitleEntered
 
-  
   var promise = new Promise(function(resolve, reject) {
     resolve(
         {
@@ -44,14 +43,25 @@ QUnit.test("button click sends updated title to DB", function() {
   });
 
   let stub = sinon.stub(window, 'fetch').returns(promise);
-  document.querySelector('#createListButton').click();
-
+  await new Promise(
+    ( resolve ) => {
+      document.querySelector('#createListButton').click()
+      setTimeout(() => resolve(), 500)
+    })
+  
   //grab the button
   console.log(`#editTitleButton${idForList}`)
   var button = document.querySelector(`#editTitleButton${idForList}`)
   //click the button
-  button.click();
+  await new Promise(
+    ( resolve ) => {
+      button.click();
+      setTimeout(() => resolve(), 500)
+    })
 
+  var title = document.querySelector(`#title${idForList}`)
+
+  ok(title.getAttribute('contenteditable'))
   //grab the h6, update value
   //click/enter the h6 
   //verify that request to backend was sent.
@@ -62,72 +72,73 @@ QUnit.test("button click sends updated title to DB", function() {
   // equal(spy.args[0][1], "http://localhost:8080/list");
   spy.restore();
   stub.restore();
+  
 });
 /* ------------- WIP test failing after api.js code refactor.  -------------- */
-// QUnit.test("User input entered populates list title on card after button click", function(assert) {
+QUnit.test("User input entered populates list title on card after button click", function(assert) {
 
-//   var done = assert.async();
-//   card.called = false;
-//   currId = 9909;
-//   var listTitleEntered = "New List";
-//   const input = document.querySelector('#titleInput');
-//   input.value = listTitleEntered;
+  var done = assert.async();
+  card.called = false;
+  currId = 9909;
+  var listTitleEntered = "New List";
+  const input = document.querySelector('#titleInput');
+  input.value = listTitleEntered;
 
-//   var promise = new Promise(function(resolve, reject) {
-//     resolve(
-//         {
-//           status: 201,
-//           json() { return new Promise(function(resolve, reject)
-//             {
-//               resolve ({"title": listTitleEntered });
-//               reject();
-//             });
-//           }
-//         }
-//     );
-//     reject();
-//   });
+  var promise = new Promise(function(resolve, reject) {
+    resolve(
+        {
+          status: 201,
+          json() { return new Promise(function(resolve, reject)
+            {
+              resolve ({"title": listTitleEntered });
+              reject();
+            });
+          }
+        }
+    );
+    reject();
+  });
 
-//   let spy = sinon.spy(card, "createListCard");
-//   let stub = sinon.stub(window, 'fetch').returns(promise);
+  let spy = sinon.spy(card, "createListCard");
+  let stub = sinon.stub(window, 'fetch').returns(promise);
 
-//   document.querySelector('#createListButton').click();
+  document.querySelector('#createListButton').click();
 
-//   setTimeout(function() {
-//     currId = 0;
-//     equal(spy.calledWithExactly(9909, listTitleEntered), true);
-//     stub.restore();
-//     spy.restore();
-//     done();
-//   });
-// });
+  setTimeout(function() {
+    currId = 0;
+    equal(spy.calledWithExactly(9909, listTitleEntered), true);
+    stub.restore();
+    spy.restore();
+    done();
+  });
+});
 
-// QUnit.test("list title field initially 'Enter title here' for a new card after other cards have been added", function(){
-//   const input = document.querySelector('#titleInput');
+QUnit.test("list title field initially 'Enter title here' for a new card after other cards have been added", function(){
+  const input = document.querySelector('#titleInput');
 
-//   equal(input.placeholder, "Enter title here", "Initial list title placeholder is presents");
-// });
+  equal(input.placeholder, "Enter title here", "Initial list title placeholder is presents");
+});
 
-// QUnit.test("Card was not created with 304 status", function(assert) {
-//   var done = assert.async();
-//   var listTitleEntered = "New List";
-//   const input = document.querySelector('#titleInput');
-//   input.value = listTitleEntered;
+QUnit.test("Card was not created with 304 status", function(assert) {
+  var done = assert.async();
+  var listTitleEntered = "New List";
+  const input = document.querySelector('#titleInput');
+  input.value = listTitleEntered;
 
-//   var promise = new Promise(function(resolve, reject) {
-//       resolve({"status": 304, "title": listTitleEntered});
-//   });
+  var promise = new Promise(function(resolve, reject) {
+      resolve({"status": 304, "title": listTitleEntered});
+  });
   
-//   let spy = sinon.spy(card, "createListCard");
-//   let stub = sinon.stub(window, 'fetch').returns(promise);
+  let spy = sinon.spy(card, "createListCard");
+  let stub = sinon.stub(window, 'fetch').returns(promise);
   
-//   document.querySelector('#createListButton').click();
+  document.querySelector('#createListButton').click();
 
-//   setTimeout(function() {
-//     equal(spy.called, false);
-//     stub.restore();
-//     spy.restore();
-//     done();
-//   });
+  setTimeout(function() {
+    equal(spy.called, false);
+    stub.restore();
+    spy.restore();
+    done();
+  });
 
-// });
+});
