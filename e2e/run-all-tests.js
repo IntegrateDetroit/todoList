b87726744bc25541ce4fdfacf4ac4afafbcadc93
Cole Wyman <cole@integral.io>
2018-11-12 20:13:44 -0500

    update run-all-tests to run synchronously

--- e2e/run-all-tests.js
+++ e2e/run-all-tests.js
@@ -2,61 +2,81 @@ const newman = require('newman');
 const fs = require('fs');
 const os = require('os');
 
-function printUsage(){
+
+const getConfig = ( collection, environment ) => {
+  if (os.platform == 'darwin')
+    return {
+      collection: `./collections/${collection}`,
+      environment: `./env/${environment}`,
+      delayRequest: 2000,
+      reporters: 'cli'
+  } 
+  return {
+      collection: `.\\collections\\${collection}`,
+      environment: `.\\env\\${environment}`,
+      delayRequest: 2000,
+      reporters: 'cli'
+    }
+}
+
+const parseConfigFile = ( configFile ) => {
+  if(os.platform == "darwin") 
+    return configFile.split(";\n")
+  return configFile.split(";\r\n")
+}
+
+const printUsage = () => {
   console.log('USAGE: node test-runner.js <collection-name> <environment-name>');
 }
 
-const runTestFor = (collection, env) => {
-  console.log(`Running test: ${collection} ${env}`)
-  console.log(collection)
-  console.log(env)
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+const runTestFor = async (collection, env) => {
   if(!(collection && env)) {
     printUsage();
     process.exit(1);
   }
-  let runConfig;
-  console.log("PLATFORM " + os.platform);
-  if (os.platform == 'darwin'){
-    runConfig = {
-      collection: './collections/' + collection,
-      environment: './env/' + env,
-      delayRequest: 2000,
-      reporters: 'cli'
-    };
-  } else {
-    runConfig = {
-      collection: '.\\collections\\' + collection,
-      environment: '.\\env\\' + env,
-      delayRequest: 2000,
-      reporters: 'cli'
-    }
-  }
+
+  const runConfig = getConfig( collection, env )
   
-  newman.run(runConfig, (err, summary, run) => {
-    if (err) {
-      console.log(`ERROR for ${ collection }`, err);
-      process.exit(1);
-    }
-    if (summary.run.failures && summary.run.failures.length > 0) {
-      console.log(`FAILURE for ${ collection }`, summary.run.failures);
-      process.exit(1);
-    }
-  });
+  return new Promise(
+    ( resolve, reject ) => {
+      newman.run(
+        runConfig, 
+        (err, summary) => {
+          // console.log(`Running test: ${collection} ${env}`)
+          if (err) {
+            console.log(`ERROR for ${ collection }`, err);
+            reject()
+          }
+          if (summary.run.failures && summary.run.failures.length > 0) {
+            console.log(`FAILURE for ${ collection }`, summary.run.failures);
+            reject()
+          }
+          resolve()
+        })
+    })
   
 }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 let text = fs.readFileSync( './testPairs.txt', { encoding: 'UTF-8' } )
+let pairs = parseConfigFile( text );
 
-let pairs;
-if(os.platform == "darwin"){
-  pairs = text.split(";\n");
-}else{
-  pairs = text.split(";\r\n")
-}
+(async () => {
 
-pairs.forEach(pair => {
-  let [ collection, environment ] = pair.split( ', ' )
-  environment = environment.replace( ';', '' );
+  for( let index in pairs ) {
+    let pair = pairs[ index ]
 
-  runTestFor( collection, environment );
-});
+    let [ collection, environment ] = pair.split( ', ' )
+    environment = environment.replace( ';', '' );
+
+    try {
+      await runTestFor( collection, environment );
+    } catch( exception ) {
+      process.exit( 1 )
+    }
+  }
+
+})()
